7e3974935594f1d9471c4fab638c218b16f72b2a
RainMaker3363 <jsc22637@gmail.com>
2016-09-17 22:29:44 +0900

    C# 기반 서버 모듈 Week 3
    GameServer_Moudle을 추가하여 기본적인 퍼즐게임 프레임워크 제작

---
+++ OnlineGameServer_Module(C#)/Server_Network_Module/GameServer_Module/VirusWarGameServer/CPlayer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirusWarGameServer
+{
+    using GameServer_Module;
+
+    // 게임을 플레이하는 플레이어에 관련된 내용을 담은 클래스
+    // 플레이어 인덱스와 현재 보유하고 있는 세균의 개수를 관리함
+    class CPlayer
+    {
+        CGameUser owner;
+
+        public byte player_index { get; private set; }
+        public List<short> viruses { get; private set; }
+
+        public CPlayer(CGameUser user, byte player_index)
+        {
+            this.owner = user;
+            this.player_index = player_index;
+            this.viruses = new List<short>();
+        }
+
+        public void reset()
+        {
+            this.viruses.Clear();
+        }
+
+        public void add_cell(short position)
+        {
+            this.viruses.Add(position);
+        }
+
+        public void remove_cell(short position)
+        {
+            this.viruses.Remove(position);
+        }
+
+        public void send(CPacket msg)
+        {
+            this.owner.send(msg);
+            CPacket.destroy(msg);
+        }
+
+        public void send_for_broadcast(CPacket msg)
+        {
+            this.owner.send(msg);
+        }
+
+        public int get_virus_count()
+        {
+            return this.viruses.Count;
+        }
+    }
+}
